354c145d54573eec7e80cf39af1295a068db8845
Shankar006-ss <113160165+Shankar006-ss@users.noreply.github.com>
2023-04-11 18:46:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 
 
-module.exports = {
-    sayHello:()=>{
-        console.log("This is an function");
-    },
-    new:()=>{
-      console.log("Separate");
-    }
+// module.exports = {
+//     sayHello:()=>{
+//         console.log("This is an function");
+//     },
+//     new:()=>{
+//       console.log("Separate");
+//     }
+// }
+
+import React,{useState,useEffect} from 'react';
+import {View,Text} from 'react-native';
+
+const myNpm = ()=>{
+
+  useEffect=(()=>{
+    console.log("This is initial time");
+  })
+  return(
+   null
+  )
 }
+
+export default myNpm;
